5c8dfb5f0be977d8174085cc7c7481d9d386e82e
S4cha <sachadso@gmail.com>
2020-04-07 13:25:30 +0200

    Creates Xcode Project

---
+++ Eurovision2020/Eurovision2020/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Eurovision2020
+//
+//  Created by Sacha DSO on 07/04/2020.
+//  Copyright © 2020 MarsacProductions. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = HomeVC()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ Eurovision2020/Eurovision2020/HomeVC.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Eurovision2020
+//
+//  Created by Sacha DSO on 07/04/2020.
+//  Copyright © 2020 MarsacProductions. All rights reserved.
+//
+
+import UIKit
+
+class HomeView: UView {
+    
+}
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        view.backgroundColor = .yellow
+    }
+}
+
